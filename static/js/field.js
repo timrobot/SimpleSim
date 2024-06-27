a95e1cdfcd5f567de0af70bf858e8856ec30f5b5
Timothy Yong <tyong_23@hotmail.com>
2024-06-27 01:21:24 -0700

    add field

---
+++ static/js/field.js
@@ -0,0 +1,93 @@
+import * as THREE from 'three';
+
+const textureLoader = new THREE.TextureLoader();
+textureLoader.crossOrigin = 'anonymous';
+
+function loadTexture(path) {
+  const filepath = `./static/assets/${path}`;
+  const texture = textureLoader.load(filepath);
+  texture.colorSpace = THREE.SRGBColorSpace;
+  texture.minFilter = THREE.NearestFilter;
+  texture.magFilter = THREE.NearestFilter;
+  return texture;
+}
+
+class Field extends THREE.Group {
+  constructor() {
+    super();
+    this.tag16h5 = {};
+  }
+
+  initMeshes(physx) {
+    const in2m = (x) => x * 0.0254;
+
+    const groundGeometry = new THREE.BoxGeometry(50, 1, 50);
+    const groundMaterial = new THREE.MeshLambertMaterial({color: 0xdacfa3});
+    const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+    ground.position.set(0, -0.5, 0);
+    ground.castShadow = true;
+    ground.receiveShadow = true;
+    this.add(ground);
+    physx.add(ground, {collideGroup: 1, collideWith: 0xFF});
+
+    const walls = [];
+    const wallGeometry = new THREE.BoxGeometry(in2m(145), in2m(13.5), in2m(1));
+    const wallMaterial = new THREE.MeshLambertMaterial({color: 0xd6d9cc});
+    for (let i = 0; i < 4; i++) {
+      walls.push(new THREE.Mesh(wallGeometry, wallMaterial));
+    }
+    walls[0].position.set( 0, in2m(6.75), in2m(73));
+    walls[1].position.set( in2m(73), in2m(6.75), 0);
+    walls[1].rotateY(Math.PI / 2);
+    walls[2].position.set( 0, in2m(6.75),-in2m(73));
+    walls[3].position.set(-in2m(73), in2m(6.75), 0);
+    walls[3].rotateY(Math.PI / 2);
+    for (const wall of walls) {
+      wall.castShadow = true;
+      wall.receiveShadow = true;
+      this.add(wall);
+      physx.add(wall, {collideGroup: 1, collideWith: 0xFF,
+        geometry: "BoxGeometry", parameters: {width: in2m(145), height: in2m(13.5), depth: in2m(1)}});
+    }
+
+    const black_material = new THREE.MeshLambertMaterial({color: 0x080808});
+    for (let i = 0; i < 30; i++) {
+      const texture = loadTexture(`tag16h5_000${i < 10 ? '0': ''}${i}.png`);
+      const tag_material = new THREE.MeshLambertMaterial({map: texture});
+      const frame_material = [black_material, black_material, tag_material, black_material, black_material, black_material];
+      const frame_geometry = new THREE.BoxGeometry(in2m(4), in2m(0.5), in2m(4));
+      const frame_mesh = new THREE.Mesh(frame_geometry, frame_material);
+      this.tag16h5[i] = frame_mesh;
+    }
+
+    for (let i = 0; i < 12; i++) {
+      this.tag16h5[i].rotateX(Math.PI / 2);
+      if (i < 3) {
+        this.tag16h5[i].rotateZ(-Math.PI / 2);
+        this.tag16h5[i].position.set(in2m(-72), in2m(12), in2m(-12 - 24 * i));
+      } else if (3 <= i && i < 9) {
+        this.tag16h5[i].position.set(in2m(-60 + 24 * (i - 3)), in2m(12), in2m(-72));
+      } else if (9 <= i && i < 12) {
+        this.tag16h5[i].rotateZ(Math.PI / 2);
+        this.tag16h5[i].position.set(in2m(72), in2m(12), in2m(-60 + 24 * (i - 9)));
+      }
+      this.add(this.tag16h5[i]);
+    }
+    for (let i = 18; i < 30; i++) {
+      this.tag16h5[i].rotateX(Math.PI / 2);
+      if (18 <= i && i < 21) {
+        this.tag16h5[i].rotateZ(-Math.PI / 2);
+        this.tag16h5[i].position.set(in2m(-72), in2m(12), in2m(12 + 24 * (i - 18)));
+      } else if (21 <= i && i < 27) {
+        this.tag16h5[i].rotateZ(Math.PI);
+        this.tag16h5[i].position.set(in2m(-60 + 24 * (i - 21)), in2m(12), in2m(72));
+      } else if (27 <= i && i < 30) {
+        this.tag16h5[i].rotateZ(Math.PI / 2);
+        this.tag16h5[i].position.set(in2m(72), in2m(12), in2m(60 - 24 * (i - 27)));
+      }
+      this.add(this.tag16h5[i]);
+    }
+  }
+};
+
+export { Field };
