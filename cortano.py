fe6239df14c17031ada99f5f9ed1423e701ba798
Timothy Yong <tyong_23@hotmail.com>
2025-02-27 18:25:10 -0800

    fix collision for chassis

--- cortano.py
+++ cortano.py
@@ -26,41 +26,50 @@ class RealsenseCamera:
     elif not env._sensor_read_active:
       motors = [x / 100. for x in env.motors]
       action = [motors[0], 0, motors[2], 0, 0, 0, 0, motors[7], 0, -motors[9]]
-      env.obs, _, __, info = env.step(action)
-      color = info["color"]
-      depth = info["depth"]
+      env.obs, env.rew,env.term, env.info = env.gym_env.step(action)
+      color = env.info["color"]
+      depth = env.info["depth"]
     return color, depth
   
 class VexController:
   def __init__(self, keys):
     self.keys = keys
 
-class VexV5(MultiplayerEnv):
-  def __init__(self, render=True):
+class VexV5:
+  def __init__(self, envname='MultiplayerEnv', render=True):
     global env
     if env is not None:
       return
-    else:
-      env = self
+    elif envname == 'CanClawboxEnv':
+      self.gym_env = CanClawboxEnv()
+    elif envname == 'MultiplayerEnv':
+      self.gym_env = MultiplayerEnv()
 
-    super().__init__(autolaunch=False, port=9999, httpport=8765)
-    if render:
-      self.render()
     self.motors = [0] * 10
-    self.obs, rew, term, info = self.step(self.motors)
-    # self.obs, info = self.reset()
+    self.obs, _, __, self.info = self.gym_env.step(self.motors)
+    if render: self.gym_env.render()
 
     global color, depth
-    color = info["color"]
-    depth = info["depth"]
+    color = self.info["color"]
+    depth = self.info["depth"]
     self._camera_read_active = False
     self._sensor_read_active = False
     self._running_in_play = False
+    self.obs = None
+    self.rew = 0.0
+    self.term = False
+    self.info = None
 
   def read(self):
+    """Return sensor values and battery life
+
+    Returns:
+        sensors: list of sensors
+        battery: battery life
+    """
     motors = [x / 100. for x in self.motors]
     action = [motors[0], 0, motors[2], 0, 0, 0, 0, motors[7], 0, -motors[9]]
-    self.obs, rew, term, info = self.step(action)
+    self.obs, self.rew, self.term, self.info = self.gym_env.step(action)
     sensors = [
       0, action[0], 0,
       0, action[9], 0,
@@ -69,24 +78,37 @@ class VexV5(MultiplayerEnv):
     ]
 
     global color, depth
-    color = info["color"]
-    depth = info["depth"]
+    color = self.info["color"]
+    depth = self.info["depth"]
 
     self._sensor_read_active = True
     return sensors, 100
   
   @property
   def controller(self):
-    return VexController(super().keys)
+    return VexController(self.gym_env.keys)
   
   def running(self):
+    """Return whether or not robot is up and running.
+
+    Returns:
+        _type_: _description_
+    """
     self._running_in_play = True
-    r = super().running()
+    r = self.gym_env.running()
     global color, depth, env
     if not self._running_in_play:
       self._running_in_play = True
     elif not self._camera_read_active and not self._sensor_read_active:
       motors = [x / 100. for x in self.motors]
       action = [motors[0], 0, motors[2], 0, 0, 0, 0, motors[7], 0, -motors[9]]
-      self.obs, _, __, ___ = self.step(action)
+      self.obs, self.rew, self.term, self.info = self.gym_env.step(action)
     return r
+  
+  def reset(self):
+    """Hidden function for simulation only. Do not use on real robot.
+
+    Returns:
+        _type_: _description_
+    """
+    return self.gym_env.reset()
