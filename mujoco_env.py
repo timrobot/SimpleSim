a3cc29f591a8967d823f2f8a221faa73cfbf7fd7
Timothy Yong <tyong_23@hotmail.com>
2025-07-18 15:52:07 -0700

    add mj clawbot

---
+++ mujoco_env.py
@@ -0,0 +1,166 @@
+from collections import deque, namedtuple
+import os
+import mujoco
+import mujoco.viewer
+import numpy as np
+
+# this is the manually implemented mujoco, it seems to work on pendulum
+
+def quaternion_to_euler(q):
+    # q should be in [x, y, z, w] format
+    w, x, y, z = q
+
+    # Roll (X-axis rotation)
+    sinr_cosp = 2 * (w * x + y * z)
+    cosr_cosp = 1 - 2 * (x * x + y * y)
+    roll = np.arctan2(sinr_cosp, cosr_cosp)
+
+    # Pitch (Y-axis rotation)
+    sinp = 2 * (w * y - z * x)
+    pitch = np.arcsin(np.clip(sinp, -1, 1))  # Clamp to avoid NaNs
+
+    # Yaw (Z-axis rotation)
+    siny_cosp = 2 * (w * z + x * y)
+    cosy_cosp = 1 - 2 * (y * y + z * z)
+    yaw = np.arctan2(siny_cosp, cosy_cosp)
+
+    return roll, pitch, yaw  # in radians
+
+def bug_fix_angles(qpos, idx=None):
+  """Fix angles to be in the range [-pi, pi]."""
+  if idx is None:
+    idx = list(range(len(qpos)))
+  for i in idx:
+    qpos[i] = np.mod(qpos[i] + np.pi, 2 * np.pi) - np.pi
+  return qpos
+
+class ClawbotCan:
+  def __init__(self, mujoco_model_path: str="env/clawbot.xml"):
+    with open(mujoco_model_path, 'r') as fp:
+      model_xml = fp.read()
+    self.model = mujoco.MjModel.from_xml_string(model_xml)
+    self.data = mujoco.MjData(self.model)
+    self.time_duration = 0.05
+
+    self.sensor_names = [self.model.sensor_adr[i] for i in range(self.model.nsensor)]
+    self.actuator_names = [mujoco.mj_id2name(self.model, mujoco.mjtObj.mjOBJ_ACTUATOR, i) for i in range(self.model.nu)]
+    self.body_names = self.model.names.decode('utf-8').split('\x00')[1:]
+
+    self._steps = 0
+    self.max_steps = 1000
+    self.observation_space = namedtuple('Box', ['high', 'low', 'shape'])
+    # self.observation_space.shape = (self.model.nsensor,)
+    self.observation_space.shape = (3,)
+    self.observation_space.low = np.full(self.observation_space.shape, float('-inf')).tolist()
+    self.observation_space.high = np.full(self.observation_space.shape, float('inf')).tolist()
+    self.action_space = namedtuple('Box', ['high', 'low', 'shape'])
+    self.action_space.shape = (self.model.nu,)
+    self.action_space.low  = self.model.actuator_ctrlrange[:,0].tolist()
+    self.action_space.high = self.model.actuator_ctrlrange[:,1].tolist()
+
+    self.viewer = None
+    self.prev_action = np.array([0.0, 0.0, 0.0, 0.0]) # ramping
+
+  def _calc_state(self):
+      # Calculate reward and termination
+      # Get sensor indices by name
+      touch_lc_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SENSOR, "touch_lc")
+      touch_rc_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SENSOR, "touch_rc")
+
+      # Read values from the sensordata array
+      touch_lc_value = self.data.sensordata[touch_lc_id]
+      touch_rc_value = self.data.sensordata[touch_rc_id]
+
+      # print(f"Left claw touch force: {touch_lc_value}")
+      # print(f"Right claw touch force: {touch_rc_value}")
+
+      objectGrabbed = touch_lc_value or touch_rc_value
+
+      # Can position
+      can1_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_BODY, "can1")
+      can1_pos = self.data.xpos[can1_id]  # [x, y, z] in world frame
+
+      # Claw position
+      claw_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SITE, "virtual_claw_target")
+      claw_pos = self.data.xpos[claw_id]  # [x, y, z] in world frame
+
+      dx = claw_pos[0] - can1_pos[0]
+      dy = claw_pos[1] - can1_pos[1]
+      dz = claw_pos[2] - can1_pos[2]
+      distance = np.sqrt(dx * dx + dy * dy + dz * dz)
+      heading = np.arctan2(dy, dx) + np.pi/2
+      # print("Distance:", dist, "Heading:", heading)
+
+      roll, pitch, yaw = quaternion_to_euler(self.data.xquat[claw_id])
+      # print("Yaw:", yaw)
+      # yaw 0 is North, -pi is East, pi is West, 2pi is South
+
+      dtheta = bug_fix_angles([yaw - heading])[0]
+      # print("Dtheta:", dtheta)
+
+      return np.array([distance, dtheta, objectGrabbed]), np.concatenate([np.array([dtheta, dx, dy]), claw_pos], -1)
+
+  def reward(self, state, action):
+    distance, dtheta, objectGrabbed = state
+    return -distance - np.abs(dtheta) + int(objectGrabbed) * 50
+
+  def terminal(self, state, action):
+    _, __, objectGrabbed = state
+    return self._steps >= 1000 or objectGrabbed or np.cos(state[1]) < 0
+
+  def reset(self):
+    self.prev_action = np.array([0.0, 0.0, 0.0, 0.0]) 
+    """Reset the environment to its initial state."""
+    self._steps = 0
+    mujoco.mj_resetData(self.model, self.data)
+
+    # set a new can position
+    can1_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_BODY, "can1")
+    can1_jntadr = self.model.body_jntadr[can1_id]
+    can1_qposadr = self.model.jnt_qposadr[can1_jntadr]
+
+    pos = (0, 0)
+    while np.sqrt(pos[0] * pos[0] + pos[1] * pos[1]) < 0.3:
+      pos = (np.random.uniform(-0.75, 0.75), np.random.uniform(0.1, 0.75))
+    self.data.qpos[can1_qposadr+0] = pos[0]
+    self.data.qpos[can1_qposadr+1] = pos[1]
+
+    bug_fix_angles(self.data.qpos)
+    mujoco.mj_forward(self.model, self.data)
+    bug_fix_angles(self.data.qpos)
+    sensor_values = self.data.sensordata.copy()
+    return self._calc_state()[0]
+
+  def step(self, action, time_duration=0.05):
+    # for now, disable arm
+    action[2] = 0
+    action[3] = action[3] / 2 - 0.5
+
+    self.prev_action = action = \
+      np.clip(np.array(action) - self.prev_action, -0.25, 0.25) + self.prev_action
+    for i, a in enumerate(action):
+      self.data.ctrl[i] = a
+    t = time_duration
+    while t - self.model.opt.timestep > 0:
+      t -= self.model.opt.timestep
+      bug_fix_angles(self.data.qpos)
+      mujoco.mj_step(self.model, self.data)
+      bug_fix_angles(self.data.qpos)
+    sensor_values = self.data.sensordata.copy()
+    s, info = self._calc_state()
+    obs = s
+    self._steps += 1
+    reward_value = self.reward(s, action)
+    terminal_value = self.terminal(s, action)
+
+    return obs, reward_value, terminal_value, info
+
+  def render(self):
+    """Render the environment."""
+    if self.viewer is None:
+      self.viewer = mujoco.viewer.launch_passive(self.model, self.data)
+    if self.viewer.is_running():
+      self.viewer.sync()
+    else:
+      self.viewer.close()
+      self.viewer = mujoco.viewer.launch_passive(self.model, self.data)
