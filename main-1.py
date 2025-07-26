f1a3ec12073cfe6c5abfc55bc0cb4280ca64e18a
Chun Wu <chunw@nvidia.com>
2025-07-25 18:40:47 -0700

    added pyapriltags to requirements.txt

---
+++ main-1.py
@@ -0,0 +1,18 @@
+# from cortano import RealsenseCamera, VexV5
+
+# if __name__ == "__main__":
+#   camera = RealsenseCamera()
+#   robot = VexV5()
+
+#   while robot.running():
+#     # Enable on physical robot
+#     # color, depth = camera.read()
+#     # sensors, battery = robot.read()
+
+#     keys = robot.controller.keys
+#     y = keys["w"] - keys["s"]
+#     x = keys["d"] - keys["a"]
+#     robot.motor[0] = (y + x) * 50
+#     robot.motor[9] = (y - x) * 50
+#     robot.motor[7] = (keys["p"] - keys["l"]) * 100
+#     robot.motor[2] = (keys["o"] - keys["k"]) * 100
---
+++ main-2.py
@@ -0,0 +1,19 @@
+from cortano import RealsenseCamera, VexV5
+
+LEFT = 0
+RIGHT = 9
+ARM = 7
+CLAW = 2
+
+if __name__ == "__main__":
+    camera = RealsenseCamera()
+    robot = VexV5()
+
+    # Get camera parameters (use values set from camera)
+    # If connected to the camera you can get more accurate values by
+    # running rs-enumerate-devices -c
+    intrinsics = [camera.fx, camera.fy, camera.cx, camera.cy]
+
+
+    while robot.running():
+        pass
